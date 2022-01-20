cbd01dff87c65637da5e5937e49569c704a783f9
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2022-01-20 10:39:42 +0800

    第二次作业 使用storyboard布局通用框架
    使用了UINavigationController 和 UITabBarController 完成了简单页面的布局

---
+++ General framework/General framework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  General framework
+//
+//  Created by Magic Keegan on 1/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
